d36709bd35b034c42a416575a01a91c7f2bc2b23
parthibhan <parthibhank82@gmail.com>
2015-07-15 15:46:52 +0530

    removed imports

--- src/com/blogapplication/BlogController.java
+++ src/com/blogapplication/BlogController.java
@@ -2,7 +2,6 @@ package com.blogapplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.jdo.Query;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
